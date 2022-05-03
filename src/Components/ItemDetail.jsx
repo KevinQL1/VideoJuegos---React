2d92b003ed2c4cbb0f90ccfe4af072134928b02c
KevinQL1 <kevinquintero045@gmail.com>
2022-05-03 09:01:06 -0500

    Agregando ITEMDETAIL a el proyecto final

---
+++ src/Components/ItemDetail.jsx
@@ -0,0 +1,59 @@
+import ItemCount from "./ItemCount";
+
+const ItemDetail = ({ findGames }) => {
+  const estilos = {
+    width: "95%",
+  };
+
+  return (
+    <>
+      <div style={estilos} className="mx-auto">
+        <div className="card card-side mt-40 mb-20 mx-auto shadow-xl bg-indigo-300 glass">
+          <figure>
+            <img
+              style={{ height: "34rem", width: "100%" }}
+              src={findGames.img}
+              alt="Imagenes videoJuegos"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title font-black mx-auto">{findGames.name}</h2>
+            <h2 className="card-title">Description</h2>
+            <div>
+              <ol className="menu menu-horizontal mr-1">
+                {findGames.description}
+              </ol>
+            </div>
+            <h2 className="card-title">Genders</h2>
+            <div>
+              <li className="menu menu-horizontal mr-1">{findGames.genres}</li>
+            </div>
+            <h2 className="card-title">Plataforms</h2>
+            <div>
+              <li className="menu menu-horizontal mr-1">
+                {findGames.plataformas}
+              </li>
+            </div>
+            <h2 className="card-title">Lenguage</h2>
+            <div>
+              <li className="menu menu-horizontal mr-1">
+                {findGames.lenguage}
+              </li>
+            </div>
+            <h2 className="card-title">Price</h2>
+            <div>
+              <li className="menu menu-horizontal mr-1">
+                {"$" + findGames.price + " COP"}
+              </li>
+            </div>
+            <div className="card-actions justify-end">
+              <ItemCount />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ItemDetail;
--- src/Components/ItemDetailContainer.jsx
+++ src/Components/ItemDetailContainer.jsx
@@ -1,68 +1,20 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { dataGames } from "../data/dataGames";
-import ItemCount from "./ItemCount";
+import ItemDetail from "./ItemDetail";
 
 const ItemDetailContainer = () => {
   const { gamesId } = useParams();
   const [gamesDetail, setGamesDetail] = useState({});
 
   useEffect(() => {
-      setGamesDetail(dataGames.find((idFind) => idFind.id === gamesId));
+    setGamesDetail(dataGames.find((idFind) => idFind.id === gamesId));
   }, [gamesId]);
 
-  const estilos = {
-    width: "95%",
-  };
-
   return (
-    <>
-      <div style={estilos} className="mx-auto">
-        <div className="card card-side mt-40 mb-20 mx-auto shadow-xl bg-indigo-300 glass">
-          <figure>
-            <img
-              style={{height: "34rem", width: "100%"}}
-              src={gamesDetail.img}
-              alt="Imagenes videoJuegos"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title font-black mx-auto">{gamesDetail.name}</h2>
-            <h2 className="card-title">Description</h2>
-            <div>
-              <ol className="menu menu-horizontal mr-1">
-                {gamesDetail.description}
-              </ol>
-            </div>
-            <h2 className="card-title">Genders</h2>
-            <div>
-              <li className="menu menu-horizontal mr-1">
-                {gamesDetail.genres}
-              </li>
-            </div>
-            <h2 className="card-title">Plataforms</h2>
-            <div>
-              <li className="menu menu-horizontal mr-1">
-                {gamesDetail.plataformas}
-              </li>
-            </div>
-            <h2 className="card-title">Lenguage</h2>
-            <div>
-              <li className="menu menu-horizontal mr-1">
-                {gamesDetail.lenguage}
-              </li>
-            </div>
-            <h2 className="card-title">Price</h2>
-            <div>
-              <li className="menu menu-horizontal mr-1">{"$" + gamesDetail.price + " COP"}</li>
-            </div>
-            <div className="card-actions justify-end">
-              <ItemCount />
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+    <div>
+      <ItemDetail findGames={ gamesDetail } />
+    </div>
   );
 };
 
